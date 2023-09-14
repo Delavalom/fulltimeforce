fafd4fab8055fe95c5dc41f903e5c6322c4f8db7
Luis Angel Arvelo <laarvelo@icloud.com>
2023-09-14 14:18:49 +0000

    add query params

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Commit } from './entities/commit.entity';
+import { ListAllEntities } from './dtos/commit.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getAll(): Promise<Commit[]> {
-    return this.appService.getCommits();
+  async getAll(@Query() query: ListAllEntities): Promise<Commit[]> {
+    return this.appService.getCommits(query.page, query.limit);
   }
 }
--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -1,24 +1,38 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, ServiceUnavailableException } from '@nestjs/common';
 import type { AxiosError } from 'axios';
-import { catchError, firstValueFrom } from 'rxjs';
-import { Commit } from './entities/commit.entity';
+import { catchError, firstValueFrom, map } from 'rxjs';
+import type { Commit } from './entities/commit.entity';
+import { GithubCommit } from './types';
 
 @Injectable()
 export class AppService {
   constructor(private readonly httpService: HttpService) {}
-  async getCommits(): Promise<Commit[]> {
+  async getCommits(page = 1, limit = 30): Promise<Commit[]> {
     const commits = await firstValueFrom(
       this.httpService
-        .get<Commit[]>(
-          'https://api.github.com/repos/Delavalom/fulltimeforce/commits',
+        .get<GithubCommit[]>(
+          `https://api.github.com/repos/Delavalom/fulltimeforce/commits?page=${page}&per_page=${limit}`,
         )
         .pipe(
+          map((res) =>
+            res.data.map(({ sha, commit, html_url, author }) => {
+              return {
+                sha: sha,
+                author: commit.author.name,
+                date: commit.author.date,
+                message: commit.message,
+                verified: commit.verification.verified,
+                html_url: html_url,
+                avatar_url: author.avatar_url,
+              };
+            }),
+          ),
           catchError((error: AxiosError) => {
-            throw error;
+            throw new ServiceUnavailableException(error.message);
           }),
         ),
     );
-    return commits.data;
+    return commits;
   }
 }
