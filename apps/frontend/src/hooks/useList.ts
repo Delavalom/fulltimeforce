2e668629bea7a20a077264cb83ad95bc95dcce5e
Luis Angel Arvelo <laarvelo@icloud.com>
2023-09-14 15:52:44 +0000

    react-query setup

---
+++ apps/frontend/src/hooks/useList.ts
@@ -0,0 +1,14 @@
+import { Commit } from "@frontend/components/data-list/data-list"
+import { useQuery } from "react-query"
+
+export function useList(page = 1, limit = 30) {
+    const query = useQuery<Commit[]>(['commit'], async () => {
+    const response = await fetch(`${import.meta.env.VITE_PUBLIC_NESTJS_SERVER}?page=${page}&limit=${limit}`)
+    if (!response.ok) {
+      console.log('Network response was not ok')
+    }
+    return response.json()
+  })
+
+  return query
+}
